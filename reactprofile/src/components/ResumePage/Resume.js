d55d79707aab05119d2c92c0bcdf25a19d2d3e31
Jody Jones <jodybjones@yahoo.com>
2020-04-04 04:18:03 -0400

    added some more info to readme file and resume file

--- reactprofile/src/components/ResumePage/Resume.js
+++ reactprofile/src/components/ResumePage/Resume.js
@@ -5,7 +5,7 @@ import git from "../github.png"
 const resume = () => {
     return (
        <div className="container">
-          <h1>Resume isComing Soon!!!</h1>
+          <h1>Resume Is Coming Soon!!!</h1>
           <p>For now check out my Linkedin and Github profiles to know more about me.</p>
         <div className="imgcontainer">
         <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/jody-jones-aaa338172/"> <img className="img" src={linked} alt="linkedin" /> </a>
