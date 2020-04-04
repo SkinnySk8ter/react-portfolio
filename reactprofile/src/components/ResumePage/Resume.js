b00c4b94ce9bad9b6796d3a13fcf904ae9edec90
Jody Jones <jodybjones@yahoo.com>
2020-04-04 02:38:14 -0400

    added images to resume page with links

--- reactprofile/src/components/ResumePage/Resume.js
+++ reactprofile/src/components/ResumePage/Resume.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import linked from "../0.png"
+import git from "../github.png"
  
 const resume = () => {
     return (
-       <div>
-          <h1>Coming Soon!!!</h1>
-          <p>Contact US page body content</p>
+       <div className="container">
+          <h1>Resume isComing Soon!!!</h1>
+          <p>For now check out my Linkedin and Github profiles to know more about me.</p>
+        <div className="imgcontainer">
+        <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/jody-jones-aaa338172/"> <img className="img" src={linked} alt="linkedin" /> </a>
        </div>
+       <div className="imgcontainer">
+        <a rel="noopener noreferrer" target="_blank" href="https://github.com/SkinnySk8ter"> <img className="img" src={git} alt="github" /> </a>
+       </div>
+       
+
+      </div>
+       
     );
 }
  
