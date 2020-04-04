d93b29dbb1e1bad933f138a43286d4faaaead862
Jody Jones <jodybjones@yahoo.com>
2020-04-03 23:43:02 -0400

    creating pages for info to be displayed

---
+++ reactprofile/src/components/ResumePage/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const Resume = () => {
+    return (
+       <div>
+          <h1>Coming Soon!!!</h1>
+          <p>Contact US page body content</p>
+       </div>
+    );
+}
+ 
+export default Resume;
